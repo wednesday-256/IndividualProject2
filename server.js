84ab08c7f332c84c37a76b06ed2ed244e69e9048
Felix Wednesday <felix.dido.4@gmail.com>
2023-05-15 21:31:16 +0400

    Finished Implementing image file server

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+const fs = require("fs");
+
 const app = express();
 
-
 //database (mongo) initializations
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require("mongodb").MongoClient;
 const url = "mongodb+srv://wednesday:pacman1234@cluster0.vjw7i5w.mongodb.net/";
 
-MongoClient.connect(url, (err, client)=>{
-    db = client.db('webstore')
-})
+MongoClient.connect(url, (err, client) => {
+  db = client.db("webstore");
+});
 
-app.get('/', (req, res, next)=>{
-    res.send('Select a collection, e.g .. .. //collection/messages')
-})
+app.get;
 
 let db;
 const ObjectID = require("mongodb").ObjectID;
 
+// logger middleware function
 const logger_middleware = (req, res, next) => {
   //logs the requests to the server console
   console.log(
@@ -27,6 +28,29 @@ const logger_middleware = (req, res, next) => {
       JSON.stringify(req.body)
   );
   next();
+};
+
+//static lessons image middleware function
+const image_middleware = (req, res, next) => {
+  //checks if this is an image request
+  let check = req.url.split("/")[1] === "images";
+  if (!check) {
+    next();
+    return;
+  }
+  let filePath = path.join(__dirname, "static", req.url);
+  fs.stat("filePath", (err, fileInfo) => {
+    if (err) {
+      next();
+      return;
+    }
+    if (fileInfo.isFile()) {
+      res.sendFile(filePath);
+    } else {
+      res.status(404);
+      res.send("Image file not Found!");
+    }
+  });
 };
 
 app.use(express.json());
@@ -40,6 +64,12 @@ app.use((req, res, next) => {
 
 //adds logger middleware to the application
 app.use(logger_middleware);
+
+//adds custom static file middleware to the application
+
+app.get("/", (req, res, next) => {
+  res.send("Select a collection, e.g .. .. //collection/messages");
+});
 
 const port = process.env.PORT || 3000;
 
